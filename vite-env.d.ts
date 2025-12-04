5e945b83c4fe2304483634b9b7afd2b09919b6f5
mh1803 <mhussain18m@gmail.com>
2025-12-04 18:04:01 +0000

    Updated vite types

--- vite-env.d.ts
+++ vite-env.d.ts
@@ -1,11 +1,13 @@
 /// <reference types="vite/client" />
 
 interface ImportMetaEnv {
-  VITE_API_URL: string;
-  VITE_FRONTEND_PORT: string;
-  VITE_GOOGLE_MAPS_API_KEY: string;
+  readonly VITE_API_URL: string;
+  readonly VITE_FRONTEND_PORT: string;
+  readonly VITE_GOOGLE_MAPS_API_KEY: string;
 }
 
 interface ImportMeta {
   readonly env: ImportMetaEnv;
 }
+
+
